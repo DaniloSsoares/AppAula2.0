0e5b786849db3e788372e98c5eebddfc152a65dd
DaniloSsoares <danilo.ssoares201@gmail.com>
2023-03-15 10:21:08 -0300

    V2 acrecentamento da área de cadastro.

--- app/src/main/java/com/example/appaula20/MainActivity.kt
+++ app/src/main/java/com/example/appaula20/MainActivity.kt
@@ -11,10 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val edtNome: EditText = findViewById(R.id.edtNome)
+        val edtEndereco: EditText = findViewById(R.id.edtEndereco)
+        val edtBairro: EditText = findViewById(R.id.edtBairro)
+        val edtCidade: EditText = findViewById(R.id.edtCidade)
+        val edtCEP: EditText = findViewById(R.id.edtCEP)
+
+
         val btnCadastrar: Button = findViewById(R.id.btnCadastrar)
         btnCadastrar.setOnClickListener{
-            val toast = Toast.makeText(this, edtNome.text.toString(), Toast.LENGTH_SHORT)
+            val toast = Toast.makeText(this, edtNome.text.toString()
+                    +"\n"+ edtEndereco.text.toString()
+                    +"\n"+ edtBairro.text.toString()
+                    +"\n"+ edtCidade.text.toString()
+                    +"\n"+ edtCEP.text.toString(), Toast.LENGTH_SHORT)
             toast.show()
+        }
     }
-}}
+}
